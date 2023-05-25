53feeade2f4e07abfd8f51dd8810cb8a1456d349
Woon Koh <woonsta13@gmail.com>
2023-05-24 20:17:50 -0700

    animation added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
-  document.querySelector('.coin-tails').classList.remove('hidden')
-  const res = await fetch(`/api?student=flip`)
+  document.querySelector('#coin').classList.remove('heads' && 'tails' || 'heads' || 'tails');
+  const res = await fetch(`/api?coin=flip`)
   const data = await res.json()
 
   console.log(data);
-  document.querySelector('.flip-result').innerHTML = `${data.flip}`
+  let flipResult = data.flip
+  document.querySelector('.flip-result').innerHTML = `${flipResult}`
 
-  
+  setTimeout(function(){
+    if(flipResult === 'heads'){
+      document.querySelector('#coin').classList.add('heads');
+      console.log('it is head');
+    }
+    else{
+      document.querySelector('#coin').classList.add('tails');
+      console.log('it is tails');
+    }
+  }, 100);
 
 
- if (data.flip === 'heads') {
-  document.querySelector('.coin-tails').classList.add('hidden')
- }
+//  if (data.flip === 'heads') {
+//   document.querySelector('.coin-tails').classList.add('hidden')
+//  }
 }
